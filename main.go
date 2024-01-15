1f10ca75fc4a62a60e5b70f4699d72607305533a
Kushagra Gupta <kushagra.kingg@live.com>
2024-01-15 21:41:22 +0530

    added code to shuffle decks and read from file

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 func main() {
-	cards := newDeck()
-
-	var handSize int = 5
-	hand, remainingCards := deal(cards, handSize)
-
-	hand.print()
-	remainingCards.print()
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
 }
